c07b6ed499c04047c1a4688739db4e0a83a0c891
Julio Rodríguez <julio10489@gmail.com>
2024-05-25 18:56:56 -0500

    Cambiado: Estilos del boton al hover y agregado la opcion para ir al registro

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom"
 import { Input } from "../components/UI/Input"
 import { Layout } from "../components/UI/Layout"
+import { PUBLIC_ROUTES } from "../routes/TypesRoutes"
 
 export const LogIn: React.FC = () => {
   return (
@@ -9,11 +11,12 @@ export const LogIn: React.FC = () => {
         <Input typeInput="text" placeholder="Email" />
         <Input typeInput="password" placeholder="Password" />
         <span className="text-dark text-xl">¿Perdiste tu contraseña?</span>
-        <button className="bg-action text-white text-xl px-2 py-3 rounded-md w-full">Iniciar sesión</button>
+        <button className="bg-action  hover:bg-white hover:text-dark text-white text-l px-2 py-1 rounded-md w-full hover:border-dark border-2 border-solid border-action">Iniciar sesión</button>
         <div className="flex flex-row-reverse gap-4 justify-center w-full">
           <span className="text-l text-dark ">Recordarme</span>
           <input type="checkbox" placeholder="Recordarme" />
         </div>
+        <Link to={PUBLIC_ROUTES.SIGNIN} className="text-l hover:text-action">¿No estás registrado? <strong>Registrate</strong></Link>
       </div>
     </Layout>
   )
