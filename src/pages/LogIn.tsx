d301143cc6f87f0a6e64b8609998a74749049bb8
Julio Rodríguez <julio10489@gmail.com>
2024-05-25 18:41:22 -0500

    Agregado: Maquetacion de la Page Login

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,5 +1,20 @@
-export const LogIn: React.FC = ()=>{
+import { Input } from "../components/UI/Input"
+import { Layout } from "../components/UI/Layout"
+
+export const LogIn: React.FC = () => {
   return (
-    <h1>LogIn</h1>
+    <Layout>
+      <div className="flex flex-col justify-evenly items-center h-full w-7/12 mx-auto">
+        <h1 className="font-medium text-3xl font-mono text-dark ">Iniciar sesion</h1>
+        <Input typeInput="text" placeholder="Email" />
+        <Input typeInput="password" placeholder="Password" />
+        <span className="text-dark text-xl">¿Perdiste tu contraseña?</span>
+        <button className="bg-action text-white text-xl px-2 py-3 rounded-md w-full">Iniciar sesión</button>
+        <div className="flex flex-row-reverse gap-4 justify-center w-full">
+          <span className="text-l text-dark ">Recordarme</span>
+          <input type="checkbox" placeholder="Recordarme" />
+        </div>
+      </div>
+    </Layout>
   )
 }
