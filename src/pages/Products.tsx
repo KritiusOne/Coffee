997a1587a5739261f74f340aa262d058bfb629f5
Julio Rodríguez <julio10489@gmail.com>
2024-05-25 20:12:47 -0500

    Agregado: Maquetacion de la pagina de productos

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,7 +1,21 @@
+import { CardProduct } from "../components/UI/CardProduct"
+import { Layout } from "../components/UI/Layout"
+
 export const Products: React.FC = ()=>{
   return (
-    <h1>
-      Productos
-    </h1>
+    <Layout>
+      <h1 className="text-center text-3xl font-sans font-semibold mt-6">Productos</h1>
+      <div className="grid grid-cols-2 my-6 gap-4">
+        {
+          Array.from(Array(6), (_, i)=>{
+            return (
+              <div className="flex justify-center items-center">
+                <CardProduct key={i} image="/src/assets/descarga.jfif" price={4000} stars={3} title="Café expreso" />
+              </div>
+            )
+          })
+        }
+      </div>
+    </Layout>
   )
 }
