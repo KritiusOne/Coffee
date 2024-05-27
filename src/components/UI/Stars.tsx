e0901b64f3b81339c48cddd2f15748149874e2b0
Julio Rodríguez <julio10489@gmail.com>
2024-05-27 00:45:14 -0500

    Cambiado: Se modificó el componente Stars para poder adaptarlo mejor al tamaño del carrito

--- src/components/UI/Stars.tsx
+++ src/components/UI/Stars.tsx
@@ -1,12 +1,14 @@
 import { IconStarFilled } from '@tabler/icons-react'
-interface Props {
+import { CARD_TYPE } from '../../helpers/CardProductType'
+interface Props extends React.HTMLAttributes<HTMLElement>{
   numStars: number
+  typeCard: string
 }
-export const Stars: React.FC<Props> = ({numStars})=>{
+export const Stars: React.FC<Props> = ({numStars, typeCard,...props})=>{
   return (
-    <div className='flex flex-row gap-2'>
+    <div {...props} className={`flex flex-row gap-2 ${props.className}`}>
       {
-        Array.from(Array(5), (_,i)=> <IconStarFilled key={i} color={i < numStars ? "#E0B531": "#000"} /> )
+        Array.from(Array(5), (_,i)=> <IconStarFilled size={CARD_TYPE.PRODUCT_CART == typeCard ? 16 : 24} key={i} color={i < numStars ? "#E0B531": "#000"} /> )
       }
     </div>
   )
