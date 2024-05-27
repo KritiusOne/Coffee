b107ed9553e1ef607c2406c9082ccbbddf0dbeaa
Julio Rodríguez <julio10489@gmail.com>
2024-05-27 00:44:39 -0500

    Cambiado: Se modicaron los estilos de la card para poderse utilizar en el carrito

--- src/components/UI/CardProduct.tsx
+++ src/components/UI/CardProduct.tsx
@@ -1,24 +1,40 @@
 import { HTMLAttributes } from "react"
 import { Stars } from "./Stars"
+import { Button } from "./Button"
+import { useCartStorage } from "../../zustand/CartStorage"
+import { ProductMock } from "../../types/ProductType"
+import { CARD_TYPE } from "../../helpers/CardProductType"
+import { IconX } from '@tabler/icons-react';
 
 interface Props extends HTMLAttributes<HTMLElement>{
-  image: string
-  title: string
-  stars: number
-  price: number
+  Product: ProductMock
+  typeCard: string
 }
-export const CardProduct: React.FC<Props> = ({image, title, stars, price, ...props})=>{
+export const CardProduct: React.FC<Props> = ({Product, typeCard,...props})=>{
+  const AddCartProduct = useCartStorage(Cart => Cart.setCartProduct)
+  const DeleteCartProduct = useCartStorage(Cart=> Cart.setDeleteProduct)
+  const handleClickButton = ()=>{
+    AddCartProduct(Product)
+  }
+  const handleClickIconX = ()=>{
+    DeleteCartProduct(Product)
+  }
   return (
-    <article {...props} className={`flex flex-col justify-center items-center shadow-[0_3px_10px_rgb(0,0,0,0.2)] w-96 pb-4 ${props.className}`} >
-      <header className="w-full h-3/5">
-        <img src={image} alt={`Imagen del producto ${title}`} className="w-full h-full" />
+    <article {...props} className={`flex ${typeCard == CARD_TYPE.PRODUCT_GRID ? "flex-col justify-center pb-4" : "flex-row justify-between pr-2"} items-center shadow-[0_3px_10px_rgb(0,0,0,0.2)] max-w-96 ${props.className}`} >
+      <header className={`${typeCard == CARD_TYPE.PRODUCT_GRID ? "w-full h-3/5" : "h-full w-2/6"}`}>
+        <img src={Product.thumbnail} alt={`Imagen del producto ${Product.title}`} className="w-full h-full" />
       </header>
-      <main className="flex flex-col justify-center items-center">
-        <h3 className="font-semibold text-dark text-l"> {title} </h3>
-        <Stars numStars={stars} />
-        <strong className="text-xl text-dark"> ${price} </strong>
+      <main className="flex flex-col justify-center items-center py-1 gap-1">
+        <h3 className="font-semibold text-dark text-l"> {Product.title} </h3>
+        <Stars numStars={Product.rating} typeCard={CARD_TYPE.PRODUCT_CART} />
+        <strong className={`text-dark ${CARD_TYPE.PRODUCT_GRID == typeCard ? "text-xl" : "text-l"}`}> ${Product.price} </strong>
       </main>
-      <button className="max-w-2/12 rounded-sm px-3 py-1 bg-action text-white text-xl">Agregar al Carrito</button>
+      {
+        typeCard == CARD_TYPE.PRODUCT_GRID ?
+        <Button onClick={handleClickButton} >Agregar al Carrito</Button>
+        : <IconX onClick={handleClickIconX} className="hover:text-action cursor-pointer" />
+
+      }
     </article>
   )
 }
