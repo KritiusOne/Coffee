2fe3dc7abace708a058300310f31828d044c0e8f
Julio Rodríguez <julio10489@gmail.com>
2024-05-28 18:49:19 -0500

    Cambiado: Agregada manejo del evento para poder dirigirse a la pantalla de detalles del producto

--- src/components/UI/CardProduct.tsx
+++ src/components/UI/CardProduct.tsx
@@ -5,6 +5,8 @@ import { useCartStorage } from "../../zustand/CartStorage"
 import { ProductMock } from "../../types/ProductType"
 import { CARD_TYPE } from "../../helpers/CardProductType"
 import { IconX } from '@tabler/icons-react';
+import { useNavigate } from "react-router"
+import { PUBLIC_ROUTES } from "../../routes/TypesRoutes"
 
 interface Props extends HTMLAttributes<HTMLElement>{
   Product: ProductMock
@@ -13,14 +15,25 @@ interface Props extends HTMLAttributes<HTMLElement>{
 export const CardProduct: React.FC<Props> = ({Product, typeCard,...props})=>{
   const AddCartProduct = useCartStorage(Cart => Cart.setCartProduct)
   const DeleteCartProduct = useCartStorage(Cart=> Cart.setDeleteProduct)
-  const handleClickButton = ()=>{
+  const navegate = useNavigate();
+  const handleClickButton = (e: React.MouseEvent)=>{
     AddCartProduct(Product)
+    e.stopPropagation()
   }
-  const handleClickIconX = ()=>{
+  const handleClickIconX = (e: React.MouseEvent)=>{
     DeleteCartProduct(Product)
+    e.stopPropagation()
+  }
+  const handleClickCard = ()=>{
+    const UrlParams = new URLSearchParams();
+    UrlParams.set("id", Product.id.toString())
+    const baseURL = PUBLIC_ROUTES.PRODUCT_DETAILS.split(":")
+    const finalURL = `${baseURL[0]}${baseURL[1]}?${UrlParams.toString()}`
+    navegate(finalURL)
+    console.log(finalURL)
   }
   return (
-    <article {...props} className={`flex ${typeCard == CARD_TYPE.PRODUCT_GRID ? "flex-col justify-center pb-4" : "flex-row justify-between pr-2"} items-center shadow-[0_3px_10px_rgb(0,0,0,0.2)] max-w-96 ${props.className}`} >
+    <article {...props} onClick={handleClickCard} className={`flex ${typeCard == CARD_TYPE.PRODUCT_GRID ? "flex-col justify-center pb-4" : "flex-row justify-between pr-2"} items-center shadow-[0_3px_10px_rgb(0,0,0,0.2)] max-w-96 cursor-pointer ${props.className}`} >
       <header className={`${typeCard == CARD_TYPE.PRODUCT_GRID ? "w-full h-3/5" : "h-full w-2/6"}`}>
         <img src={Product.thumbnail} alt={`Imagen del producto ${Product.title}`} className="w-full h-full" />
       </header>
