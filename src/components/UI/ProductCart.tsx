7813adfa81fa8fdc1df3a4e7a8c2d81822e706d4
Julio Rodríguez <julio10489@gmail.com>
2024-05-30 16:21:04 -0500

    Cambiado: Cambios del tipo del Mock a la response de la API

--- src/components/UI/ProductCart.tsx
+++ src/components/UI/ProductCart.tsx
@@ -10,37 +10,37 @@ interface Props {
 export const ProductCart: React.FC<Props> = ({ product }) => {
   const setNewCantidad = useCartStorage(Storage => Storage.setCantidad)
   const [cantidad, setCantidad] = useState<number>(product.stockSolicitado)
-  console.log( product.Product.title + " " + product.stockSolicitado)
+  console.log( product.Product.Menu.nombre_producto + " " + product.stockSolicitado)
   const handleChangeCantidad = (e: ChangeEvent<HTMLInputElement>)=>{
     const newCantidad: number =  parseInt(e.target.value)
     setCantidad(newCantidad)
   }
   const handleCickAddQuantity = ()=>{
     if(cantidad != product.stockSolicitado && cantidad > 0){
-      setNewCantidad(product.Product.id, cantidad)
+      setNewCantidad(product.Product.Menu.id_menu, cantidad)
     }
   }
   return (
     <article className="flex flex-col justify-center items-start shadow-[0_3px_10px_rgb(0,0,0,0.2)] font-mono">
       <div className="flex flex-row gap-3">
         <div className="w-2/5 h-full">
-          <img className="w-full h-full" src={product.Product.thumbnail} alt={`Imagen de ${product.Product.title}`} />
+          <img className="w-full h-full" src={product.Product.Menu.id_menu.toString()} alt={`Imagen de ${product.Product.Menu.nombre_producto}`} />
         </div>
         <div className="flex flex-col gap-4 p-2">
           <h2 className="text-xl font-semibold text-dark text-pretty">
             {
-              product.Product.title
+              product.Product.Menu.nombre_producto
             }
           </h2>
           <p className="text-md text-balance">
             {
-              product.Product.description
+              product.Product.Menu.descripcion
             }
           </p>
         </div>
       </div>
       <div className="p-4 ">
-        <span>Precio: <strong>${product.Product.price} C/U </strong></span>
+        <span>Precio: <strong>${product.Product.Menu.precio} C/U </strong></span>
         <div className="flex flex-row justify-center items-center gap-3">
           <span>Cantidad: {product.stockSolicitado} </span>
           <div className="flex flex-row justify-center gap-2">
