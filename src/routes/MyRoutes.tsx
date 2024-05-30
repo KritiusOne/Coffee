8756af92691b5ab82265898749d3bb748c7f34a2
Julio Rodríguez <julio10489@gmail.com>
2024-05-29 20:25:20 -0500

    Agregado: Nuevas rutas

--- src/routes/MyRoutes.tsx
+++ src/routes/MyRoutes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router"
-import { PUBLIC_ROUTES } from "./TypesRoutes"
+import { PRIVATE_CLIENT_ROUTES, PUBLIC_ROUTES } from "./TypesRoutes"
 import { Home } from "../pages/Home"
 import { LogIn } from "../pages/LogIn"
 import { SignIn } from "../pages/SignIn"
 import { Products } from "../pages/Products"
 import { ProductDetails } from "../pages/ProductDetails"
+import { AuthGuard } from "../guards/Auth.guard"
+import { CartBuy } from "../pages/CartBuy"
 
 export const MyRoutes: React.FC = ()=>{
   return (
@@ -14,6 +16,9 @@ export const MyRoutes: React.FC = ()=>{
       <Route path={PUBLIC_ROUTES.SIGNIN} element={<SignIn />} />
       <Route path={PUBLIC_ROUTES.PRODUCTS} element={<Products />} />
       <Route path={PUBLIC_ROUTES.PRODUCT_DETAILS} element={<ProductDetails />}  />
+      <Route element={<AuthGuard/>}>
+        <Route path={PRIVATE_CLIENT_ROUTES.CART_BUY} element={<CartBuy />} />
+      </Route>
     </Routes>
   )
 }
--- src/routes/TypesRoutes.ts
+++ src/routes/TypesRoutes.ts
@@ -5,3 +5,6 @@ export const PUBLIC_ROUTES = {
   SIGNIN: "/signin",
   PRODUCT_DETAILS: "/search-products/:product"
 }
+export const PRIVATE_CLIENT_ROUTES = {
+  CART_BUY: "/cart/buy"
+}
