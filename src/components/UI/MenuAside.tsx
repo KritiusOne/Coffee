7b17bb711d719c7f62faca0046d5388af0bc7fcc
Julio Rodríguez <julio10489@gmail.com>
2024-05-29 21:43:32 -0500

    Cambiado: Cambios en el renderizado de las opciones y su funcionalidad, queda pendiente implementar el LogOut

--- src/components/UI/MenuAside.tsx
+++ src/components/UI/MenuAside.tsx
@@ -1,11 +1,14 @@
 import { IconUser } from '@tabler/icons-react'
 import { useThemeStorage } from "../../zustand/Theme"
-import { Link } from 'react-router-dom'
-import { PUBLIC_ROUTES } from '../../routes/TypesRoutes'
+import { Link, useNavigate } from 'react-router-dom'
+import { PRIVATE_CLIENT_ROUTES, PUBLIC_ROUTES } from '../../routes/TypesRoutes'
 import { IconBuildingStore } from '@tabler/icons-react'
+import { useUserStorage } from '../../zustand/UserStorage'
 
 export const MenuAside: React.FC = ()=>{
   const ThemeConfig = useThemeStorage()
+  const UserInfo = useUserStorage(Storage => Storage.userInfo)
+  const navegate = useNavigate()
   const handleClickOverlay = () =>{
     ThemeConfig.setMenuAside(true)
     console.log(ThemeConfig.visibleMenuAside)
@@ -13,18 +16,25 @@ export const MenuAside: React.FC = ()=>{
   const handleAsideClick = (e: React.MouseEvent)=>{
     e.stopPropagation()
   }
+  const handleClickOpcion2 = ()=>{
+    if(UserInfo == undefined){
+      navegate(PUBLIC_ROUTES.SIGNIN)
+    }else{
+      console.log("LOGOUT")
+    }
+  }
   return (
     <section className='w-screen h-screen fixed top-0 left-0 bg- flex justify-center items-center md:justify-start bg-black/[0.5]' onClick={handleClickOverlay} >
       <aside className='px-6 h-4/5 w-4/5 md:h-full md:w-3/12 bg-white text-dark z-[1000]' onClick={handleAsideClick} >
         <header className='w-full flex flex-row justify-around items-center font-bold py-4'>
           <IconUser className='text-action' />
-          <h2 >Hola, ¿quieres iniciar sesion?</h2>
+          <h2 >Hola, {UserInfo == undefined ? "¿quieres iniciar sesion?" : UserInfo.first_name}</h2>
         </header>
         <main className='h-full flex flex-col justify-start items-start gap-3'>
-          <Link to={PUBLIC_ROUTES.LOGIN} className='flex flex-row w-full justify-center md:justify-start gap-2'> <IconUser className='text-action' /> Iniciar sesion </Link>
-          <Link to={PUBLIC_ROUTES.SIGNIN} className='flex flex-row w-full justify-center md:justify-start gap-2'> 
-            <IconUser className='text-action' /> Registrarse 
-          </Link>
+          <Link to={ UserInfo == undefined ? PUBLIC_ROUTES.LOGIN : PRIVATE_CLIENT_ROUTES.RESERVATION} className='flex flex-row w-full justify-center md:justify-start gap-2'> <IconUser className='text-action' /> {UserInfo == undefined ?  "Iniciar sesion" : "Hacer Reservacion"}</Link>
+          <button onClick={handleClickOpcion2} className='flex flex-row w-full justify-center md:justify-start gap-2'> 
+            <IconUser className='text-action' /> { UserInfo == undefined? "Registrarse" : "LogOut"} 
+          </button>
           <Link className='flex flex-row w-full justify-center md:justify-start gap-2' to={PUBLIC_ROUTES.PRODUCTS} > 
             <IconBuildingStore className='text-action' /> Ver Productos 
           </Link>
