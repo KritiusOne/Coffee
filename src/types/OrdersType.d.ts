bb10da6c83cb96925c18bf3cabef835ba693706f
Julio Rodríguez <julio10489@gmail.com>
2024-05-31 12:55:40 -0500

    Agregado: Types para los pedidos

---
+++ src/types/OrdersType.d.ts
@@ -0,0 +1,31 @@
+export interface Orders {
+  id_order:    number;
+  id_usuario:  number;
+  fecha_orden: Date;
+  total_price: number;
+  delivery:    Delivery;
+  sellers:     Seller[];
+}
+
+export interface Delivery {
+  id_delivery:        number;
+  state:              EstadoDelivery;
+  id_usuario:         number;
+  code_following:     string;
+  fecha_creacion:     Date;
+  fecha_finalizacion: Date | null;
+  fecha_modificacion: Date | null;
+}
+
+export interface EstadoDelivery {
+  id_states:  number;
+  name_state: string;
+}
+
+export interface Seller {
+  id_order:    number;
+  id_menu:     number;
+  numbers:     number;
+  price:       number;
+  fecha_venta: Date;
+}
