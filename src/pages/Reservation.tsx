c14c9a4f805606a2630322d9282821cd9dc946e9
Julio Rodríguez <julio10489@gmail.com>
2024-05-30 23:47:57 -0500

    Actualizacion: Actualizacion de rutas, añadida la page de Reservas

---
+++ src/pages/Reservation.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Layout } from "../components/UI/Layout";
+
+export const Reservation: React.FC = ()=>{
+  return (
+    <Layout>
+      <h1>Reservacion</h1>
+    </Layout>
+  )
+}
--- src/routes/MyRoutes.tsx
+++ src/routes/MyRoutes.tsx
@@ -7,17 +7,26 @@ import { Products } from "../pages/Products"
 import { ProductDetails } from "../pages/ProductDetails"
 import { AuthGuard } from "../guards/Auth.guard"
 import { CartBuy } from "../pages/CartBuy"
+import { AntiAuthGuard } from "../guards/AntiAuth.guard"
+import { Reservation } from "../pages/Reservation"
 
 export const MyRoutes: React.FC = ()=>{
   return (
     <Routes>
       <Route path={PUBLIC_ROUTES.HOME} element={<Home />} />
-      <Route path={PUBLIC_ROUTES.LOGIN} element={<LogIn />} />
-      <Route path={PUBLIC_ROUTES.SIGNIN} element={<SignIn />} />
       <Route path={PUBLIC_ROUTES.PRODUCTS} element={<Products />} />
-      <Route path={PUBLIC_ROUTES.PRODUCT_DETAILS} element={<ProductDetails />}  />
+      <Route path={PUBLIC_ROUTES.PRODUCT_DETAILS} element={<ProductDetails />} />
+      <Route element={<AntiAuthGuard/>}>
+        <Route path={PUBLIC_ROUTES.SIGNIN} element={<SignIn />} />
+      </Route>
+      <Route element={<AntiAuthGuard/>}>
+        <Route path={PUBLIC_ROUTES.LOGIN} element={<LogIn />} />
+      </Route>
       <Route element={<AuthGuard/>}>
         <Route path={PRIVATE_CLIENT_ROUTES.CART_BUY} element={<CartBuy />} />
+      </Route>
+      <Route element={<AuthGuard/>}>
+        <Route path={PRIVATE_CLIENT_ROUTES.RESERVATION} element={<Reservation />} />
       </Route>
     </Routes>
   )
