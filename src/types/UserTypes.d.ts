6be04287d8fb29b787d9932cffa150cbbeb2328b
Julio Rodríguez <julio10489@gmail.com>
2024-05-30 21:02:19 -0500

    Cambiado: User Types, la definicion del usuario ahora es mas correcta

--- src/types/UserTypes.d.ts
+++ src/types/UserTypes.d.ts
@@ -2,16 +2,28 @@ export interface LoginResponse {
   access_token: string;
   token_type:   string;
 }
+
 export interface User {
-  id_usuario:          number;
-  role_name:           string;
-  first_name:          string;
-  last_name:           string;
-  phone:               string;
-  email:               string;
-  city:                null;
-  state:               null;
-  country:             null;
-  address_description: null;
-  exp:                 number;
+  id_usuario: number;
+  role:       Role;
+  first_name: string;
+  last_name:  string;
+  username:   string;
+  phone:      string;
+  email:      string;
+  address:    Address;
+  exp?:        number;
+}
+
+export interface Address {
+  id_address:  number;
+  city:        string;
+  state:       string;
+  country:     string;
+  description: string;
+}
+
+export interface Role {
+  id_role:   number;
+  role_name: string;
 }
