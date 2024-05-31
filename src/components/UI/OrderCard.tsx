d8c28725863b19a23a370203e448c066e6609769
Julio Rodríguez <julio10489@gmail.com>
2024-05-31 15:22:29 -0500

    Cambios por error en prod

--- src/components/UI/OrderCard.tsx
+++ src/components/UI/OrderCard.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router"
 import { ADMIN_ROUTES } from "../../routes/TypesRoutes"
 import { useEffect, useState } from "react"
-import { useOrderStorage } from "../../zustand/OrderStorage"
 
 interface Props {
   idUser: number
@@ -14,9 +13,7 @@ interface Props {
 export const OrderCard: React.FC<Props> = ({  idUser, fechaPedido, totalPrice, codeFollowing, ultimaMoficacion, estado})=>{
   const navegate = useNavigate()
   const [nombre, setNombre] = useState("")
-  const UserWithPedidos = useOrderStorage(Storage => Storage.AllUsers)
   useEffect(()=>{
-    const [userInfo] = UserWithPedidos.filter(user => user.id_usuario == idUser)
     setNombre(codeFollowing)
   }, [])
   const handleClickCard = ()=>{
@@ -26,6 +23,7 @@ export const OrderCard: React.FC<Props> = ({  idUser, fechaPedido, totalPrice, c
     const finalURL = `${baseURL[0]}${baseURL[1]}?${UrlParams.toString()}`
     navegate(finalURL)
     console.log(finalURL)
+    console.log(idUser)
   }
   return (
     <article onClick={handleClickCard} className="flex flex-col justify-center items-start cursor-pointer border-2 border-solid border-dark px-2 py-3 font-sans hover:bg-action hover:text-white">
