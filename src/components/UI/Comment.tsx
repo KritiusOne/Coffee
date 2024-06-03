a1ad84948f6d83d7b788343091871aa3eea691d5
Julio Rodríguez <julio10489@gmail.com>
2024-06-03 18:30:09 -0500

    Agregado: Nuevo tipo de Card para distinguirlas correctamente

--- src/components/UI/Comment.tsx
+++ src/components/UI/Comment.tsx
@@ -9,13 +9,13 @@ interface Props {
 export const Comment: React.FC<Props> = ({comments, promStars})=>{
   return (
     <div className="flex flex-col justify-center items-center gap-4">
-      <Stars typeCard={CARD_TYPE.PRODUCT_GRID} numStars={promStars} />
+      <Stars typeCard={CARD_TYPE.PRODUCT_PAGE} numStars={promStars} />
       <div className="w-full flex flex-col justify-center items-center gap-2">
         {
           comments && comments.map(comment => {
             return (
               <article key={comment.id_usuario} className="flex flex-col justify-center items-center">
-                <Stars typeCard={CARD_TYPE.PRODUCT_CART} numStars={comment.rate}  />
+                <Stars typeCard={CARD_TYPE.PRODUCT_GRID} numStars={comment.rate}  />
                 <h5> {comment.description} </h5>
                 <span> {comment.fecha_creacion.toLocaleString()} </span>
               </article>
